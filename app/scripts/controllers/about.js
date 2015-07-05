7d90b6c140f1d3a5afb8a9db50559ca2a3ca98a7
Som Kasumurthi <somsekhar.tech@gmail.com>
2015-07-05 16:46:28 -0500

    adding all files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstJsAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the firstJsAppApp
+ */
+angular.module('firstJsAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstJsAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstJsAppApp
+ */
+angular.module('firstJsAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
